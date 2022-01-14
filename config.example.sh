80a2b96398daf3dfe5ca483fd5cf7b7cd1443c3c
UnkwUsr <ktoto2707043@gmail.com>
2022-01-14 20:54:58 +0300

    feat: ability to configure cmd for pre/post lock

---
+++ config.example.sh
@@ -0,0 +1,19 @@
+cmd_pre_lock() {
+    # we can set variable there and later use it in cmd_post_lock
+    ponymix is-muted; IS_WAS_MUTED=$?
+    playerctl -s status | grep -q Playing; IS_WAS_PLAYING=$?
+
+    ponymix mute > /dev/null
+    playerctl -sa pause
+
+    # example of how we can intercept internal variables
+    # notify-send "TASK_SOLVE: $TASK_SOLVE"
+    # TASK_TEXT="TASK_TEXT edited. sorry"
+}
+
+cmd_post_lock() {
+    test "$IS_WAS_MUTED" -ne 0 && \
+        ponymix unmute > /dev/null
+    test "$IS_WAS_PLAYING" -eq 0 && \
+        playerctl play
+}
